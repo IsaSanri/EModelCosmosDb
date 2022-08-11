182ed498fa582dd2a91e13e299d4b5d5843a5595
IsaSanri <isanchez@lean-tech.io>
2022-08-11 11:11:11 -0500

    Containers Info

--- todo1/Models/Item.cs
+++ todo1/Models/Item.cs
@@ -11,13 +11,25 @@ namespace todo1.Models
         [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
 
-        [JsonProperty(PropertyName = "name")]
-        public string Name { get; set; }
+        [JsonProperty(PropertyName = "container#")]
+        public string ContainerNumber { get; set; }
+
+        [JsonProperty(PropertyName = "origin")]
+        public string Origin { get; set; }
+
+        [JsonProperty(PropertyName = "destination")]
+        public string Destination { get; set; }
+
+        [JsonProperty(PropertyName = "status")]
+        public string Status { get; set; }
 
         [JsonProperty(PropertyName = "description")]
         public string Description { get; set; }
 
-        [JsonProperty(PropertyName = "isComplete")]
-        public bool Completed { get; set; }
+        [JsonProperty(PropertyName = "dimensions")]
+        public string Dimensions { get; set; }
+
+        [JsonProperty(PropertyName = "Book")]
+        public bool Book { get; set; }
     }
 }
